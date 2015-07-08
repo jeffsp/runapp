8f590bc9775027b5d6533775212ad7a26fe1c6b4
Jeff Perry <jeffsp@gmail.com>
2015-07-07 21:01:48 -0400

    get port from config

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('./config/express');
 var app = express();
-var port = 3001;
 
 app.get('/', function (req, res) {
     res.send('Hello from server.js');
 });
 
-app.listen(port);
-console.log('Server running at http://localhost:{0}/'.format(port));
+app.listen(config.port);
+console.log('Server running at http://localhost:{0}/'.format(config.port));
