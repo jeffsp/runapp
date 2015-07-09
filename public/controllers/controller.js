852c6b972af7912c7cdf42b90fb150979f4afb74
Jeff Perry <jeffsp@gmail.com>
2015-07-08 21:54:08 -0400

    add more controller code

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,5 +1,10 @@
 var runapp = angular.module('runapp', []);
 
 runapp.controller('AppCtrl',['$scope', '$http', function($scope, $http) {
-    console.log('Hello from controller');
+    console.log('Controller start');
+
+    $http.get('/users').success(function(response) {
+        console.log('Controller received the users list');
+        $scope.user_list = response;
+    });
 }]);
--- server.js
+++ server.js
@@ -6,5 +6,29 @@ var config = require('./config/config');
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/users', function (req, res) {
+    console.log('/users received a get request');
+
+    user1 = {
+        name: 'name1',
+        email: 'email1',
+        city: 'city1',
+    };
+    user2 = {
+        name: 'name2',
+        email: 'email2',
+        city: 'city2',
+    };
+    user3 = {
+        name: 'name3',
+        email: 'email3',
+        city: 'city3',
+    };
+
+    var user_list = [user1, user2, user3];
+
+    res.json(user_list);
+});
+
 app.listen(config.port);
 console.log('Server running at http://localhost:' + config.port);
